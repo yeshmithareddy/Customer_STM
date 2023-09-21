f172608a1ed8bb617d836fbd5bd8320a2bda7e60
yeshmithareddy <106657393+yeshmithareddy@users.noreply.github.com>
2023-09-21 18:41:20 +0530

    Update stm_config.py

--- stm_config.py
+++ stm_config.py
@@ -377,7 +377,7 @@ if selected_file and selected_table and selected_csv_column and selected_snowfla
 
 # mapping table
     if existing_mappings:
-        st.header("Mapping  Table")
+        st.header("Table")
         table_data = [{"Source_Columns": csv_column, "Target_Columns": snowflake_column} for mapping in existing_mappings for csv_column, snowflake_column in mapping]
         st.table(pd.DataFrame(table_data))
     else:
