72d2f0d8528ef0177688eda9460fc1bf7a526bae
yeshmithareddy <106657393+yeshmithareddy@users.noreply.github.com>
2023-09-21 15:19:05 +0530

    Update stm_config.py

--- stm_config.py
+++ stm_config.py
@@ -157,7 +157,7 @@ with right_column:
  
 
     # Add a "Preview Data" button
-    if st.button("Preview Data for Entire Snowflake Table"):
+    if st.button("Preview Data"):
         # Fetch and display data for the entire Snowflake table
         conn = snowflake.connector.connect(**connection_parameters1)
         try:
